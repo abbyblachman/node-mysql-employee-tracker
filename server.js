08459bd7f11e6f1a8028f0011bb1881dcb2aa540
Abby Blachman <ablachman@Abbys-MacBook-Pro.local>
2019-12-12 13:52:09 -0600

    adding updates to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
+    multipleStatements: true, 
     host: "localhost",
   
     // Your port; if not 3306
@@ -14,6 +15,7 @@ var connection = mysql.createConnection({
     password: "2093ftPV",
     database: "employee_db"
   });
+
   
   connection.connect(function(err) {
     if (err) throw err;
@@ -24,13 +26,16 @@ var connection = mysql.createConnection({
     inquirer
       .prompt({
         name: "action",
-        type: "rawlist",
+        type: "list",
         message: "What would you like to do?",
         choices: [
           "View all departments",
           "View all roles",
           "View all employees",
           "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update employee role",
           "Exit"
         ]
       })
@@ -43,6 +48,12 @@ var connection = mysql.createConnection({
             viewEmployees();
         } else if (answer.action === 'Add a department') {
             addDepartment();
+        } else if (answer.action === 'Add a role') {
+            addRole();
+        } else if (answer.action === 'Add an employee') {
+            addEmployee();
+        } else if (answer.action === 'Update employee role') {
+            updateRole();
         }
         else if (answer.action === 'Exit') {
             connection.end();
@@ -99,4 +110,212 @@ function addDepartment() {
         })
 }
 
+function addRole() {
+    connection.query('SELECT * FROM department', function(err, res) {
+        if (err) throw (err);
+    inquirer
+        .prompt([{
+            name: "title",
+            type: "input",
+            message: "What is the title of the new role?",
+          }, 
+          {
+            name: "salary",
+            type: "input",
+            message: "What is the salary of the new role?",
+          },
+          {
+            name: "departmentName",
+            type: "list",
+// is there a way to make the options here the results of a query that selects all departments?`
+            message: "Which department does this role fall under?",
+            choices: function() {
+                var choicesArray = [];
+                res.forEach(res => {
+                    choicesArray.push(
+                        res.name
+                    );
+                })
+                return choicesArray;
+              }
+          }
+          ]) 
+// in order to get the id here, i need a way to grab it from the departments table 
+        .then(function(answer) {
+        const department = answer.departmentName;
+        connection.query('SELECT * FROM DEPARTMENT', function(err, res) {
+        
+            if (err) throw (err);
+         let filteredDept = res.filter(function(res) {
+            return res.name == department;
+        }
+        )
+        let id = filteredDept[0].id;
+       let query = "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
+       let values = [answer.title, parseInt(answer.salary), id]
+       console.log(values);
+        connection.query(query, values,
+            function(err, res, fields) {
+            console.log(`You have added this role: ${(values[0]).toUpperCase()}.`)
+        })
+            viewRoles()
+            })
+        })
+    })
+}
+
+async function addEmployee() {
+    connection.query('SELECT * FROM role', function(err, result) {
+        if (err) throw (err);
+    inquirer
+        .prompt([{
+            name: "firstName",
+            type: "input",
+            message: "What is the employee's first name?",
+          }, 
+          {
+            name: "lastName",
+            type: "input",
+            message: "What is the employee's last name?",
+          },
+          {
+            name: "roleName",
+            type: "list",
+// is there a way to make the options here the results of a query that selects all departments?`
+            message: "What role does the employee have?",
+            choices: function() {
+             rolesArray = [];
+                result.forEach(result => {
+                    rolesArray.push(
+                        result.title
+                    );
+                })
+                return rolesArray;
+              }
+          }
+          ]) 
+// in order to get the id here, i need a way to grab it from the departments table 
+        .then(function(answer) {
+        console.log(answer);
+        const role = answer.roleName;
+        connection.query('SELECT * FROM role', function(err, res) {
+            if (err) throw (err);
+            let filteredRole = res.filter(function(res) {
+                return res.title == role;
+            })
+        let roleId = filteredRole[0].id;
+        connection.query("SELECT * FROM employee", function(err, res) {
+                inquirer
+                .prompt ([
+                    {
+                        name: "manager",
+                        type: "list",
+                        message: "Who is your manager?",
+                        choices: function() {
+                            managersArray = []
+                            res.forEach(res => {
+                                managersArray.push(
+                                    res.last_name)
+                                
+                            })
+                            return managersArray;
+                        }
+                    }
+                ]).then(function(managerAnswer) {
+                    const manager = managerAnswer.manager;
+                connection.query('SELECT * FROM employee', function(err, res) {
+                if (err) throw (err);
+                let filteredManager = res.filter(function(res) {
+                return res.last_name == manager;
+            })
+            let managerId = filteredManager[0].id;
+                    console.log(managerAnswer);
+                    let query = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+                    let values = [answer.firstName, answer.lastName, roleId, managerId]
+                    console.log(values);
+                     connection.query(query, values,
+                         function(err, res, fields) {
+                         console.log(`You have added this employee: ${(values[0]).toUpperCase()}.`)
+                        })
+                        viewEmployees();
+                        })
+                     })
+                })
+            })
+        })
+})
+}
+
+function updateRole() {
+    connection.query('SELECT * FROM employee', function(err, result) {
+        if (err) throw (err);
+    inquirer
+        .prompt([
+          {
+            name: "employeeName",
+            type: "list",
+// is there a way to make the options here the results of a query that selects all departments?`
+            message: "Which employee's role is changing?",
+            choices: function() {
+             employeeArray = [];
+                result.forEach(result => {
+                    employeeArray.push(
+                        result.last_name
+                    );
+                })
+                return employeeArray;
+              }
+          }
+          ]) 
+// in order to get the id here, i need a way to grab it from the departments table 
+        .then(function(answer) {
+        console.log(answer);
+        const name = answer.employeeName;
+        /*const role = answer.roleName;
+        connection.query('SELECT * FROM role', function(err, res) {
+            if (err) throw (err);
+            let filteredRole = res.filter(function(res) {
+                return res.title == role;
+            })
+        let roleId = filteredRole[0].id;*/
+        connection.query("SELECT * FROM role", function(err, res) {
+                inquirer
+                .prompt ([
+                    {
+                        name: "role",
+                        type: "list",
+                        message: "What is their new role?",
+                        choices: function() {
+                            rolesArray = [];
+                            res.forEach(res => {
+                                rolesArray.push(
+                                    res.title)
+                                
+                            })
+                            return rolesArray;
+                        }
+                    }
+                ]).then(function(rolesAnswer) {
+                    const role = rolesAnswer.role;
+                    console.log(rolesAnswer.role);
+                connection.query('SELECT * FROM role WHERE title = ?', [role], function(err, res) {
+                if (err) throw (err);
+                    let roleId = res[0].id;
+                    let query = "UPDATE employee SET role_id ? WHERE last_name ?";
+                    let values = [roleId, name]
+                    console.log(values);
+                     connection.query(query, values,
+                         function(err, res, fields) {
+                         console.log(`You have updated ${name}'s role to ${role}.`)
+                        })
+                        viewEmployees();
+                        })
+                     })
+                })
+            
+            //})
+       })
+})
+
+}
 
